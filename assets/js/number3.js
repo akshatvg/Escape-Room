af3560a5489b11b505458ef4a06dfbcc423b5a7f
Akshat Gupta <akshatvg23@gmail.com>
2020-09-30 03:31:53 +0530

    clue 2 changes

---
+++ assets/js/number3.js
@@ -0,0 +1,32 @@
+
+(function ($) {
+    "use strict";
+
+    // Loader
+    $(function () {
+        var loader = function () {
+            setTimeout(function () {
+                if ($('#loader').length > 0) {
+                    $('#loader').removeClass('show');
+                }
+            }, 1);
+        };
+        loader();
+    });
+
+    // Auto Init 
+    M.AutoInit();
+
+})(jQuery);
+
+// Play Music
+body.addEventListener('click', function () {
+    $("#autoPlayMusic")[0].play();
+});
+
+// Double Click
+$("#dblClick").dblclick(function () {
+    window.location.href = "/number3";
+});
+
+console.clear();
