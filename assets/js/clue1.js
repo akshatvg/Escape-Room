6e4971b6febedb7e0308c38b46930e652f0e8837
Akshat Gupta <akshatvg23@gmail.com>
2020-09-30 02:51:49 +0530

    clue 1

---
+++ assets/js/clue1.js
@@ -0,0 +1,32 @@
+
+(function ($) {
+    "use strict";
+
+    // Loader
+    $(function () {
+        var loader = function () {
+            setTimeout(function () {
+                if ($('#loader').length > 0) {
+                    $('#loader').removeClass('show');
+                }
+            }, 1);
+        };
+        loader();
+    });
+
+    // Auto Init 
+    M.AutoInit();
+
+})(jQuery);
+
+// Play Music
+body.addEventListener('click', function () {
+    $("#autoPlayMusic")[0].play();
+});
+
+// Clue 1
+$("#startJourney").click(function () {
+    window.location.href = "/clue1"
+});
+
+console.clear();
